15e9d5f0af8fed53150bc02bc55dc0ba63796d4c
limx2021 <77927466+limx2021@users.noreply.github.com>
2023-04-04 10:57:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,6 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
